d3d1d821e4778ca5ece11d79f1d38a66282ffdc4
ParvendraRajput18 <Parvendra.Rajput@adixoo.com>
2024-08-07 19:27:58 +0530

    Add initial learning content: Variables, Data Types, Strings, Numbers, User Input, Calculators, Arrays, Methods, Return Statements, If Statements

---
+++ Program.cs
@@ -0,0 +1,135 @@
+﻿// Console.WriteLine("Hello wesley");
+// Console.WriteLine("Enter your name:");
+// string name = Console.ReadLine();
+// Console.WriteLine("Hello, " + name);
+
+
+
+// Console.WriteLine("Enter your age:");
+// int age = int.Parse(Console.ReadLine()); 
+// Console.WriteLine("your age is "+ age );
+
+
+// double d = 9.78;
+// int i = (int)d; 
+
+
+// int age =20;
+// if (age>20)
+// {
+//     Console.WriteLine("you are eligible to give vote ");
+
+// }
+// else
+// {
+//     Console.WriteLine($"you cant give the vote");
+
+// }
+// Console.WriteLine($"hii whats your name");
+// string name  =Console.ReadLine();
+// Console.WriteLine($"hii {name}");
+
+
+
+// int numberOne  , numberTwo ,ans;
+// Console.WriteLine("Enter the numbers");
+
+// string one = Console.ReadLine();
+// numberOne = int.Parse(one);
+//  string two =Console.ReadLine();
+//  numberTwo = int.Parse(two);
+//  ans=numberOne+numberTwo;
+// Console.WriteLine("Sum is :" +ans);
+
+
+
+
+// int numberOne = 0;
+// int numberTwo = 0;
+// Console.WriteLine($"Enter number to muliply");
+// string enteredNumberOne = Console.ReadLine();
+// numberOne = int.Parse(enteredNumberOne); 
+// string enteredNumberTwo = Console.ReadLine();
+// numberTwo = int.Parse(enteredNumberTwo);
+// Console.WriteLine($"The Answer is "+ (numberOne*numberTwo));
+
+
+
+
+
+
+
+
+
+
+// int numberOne ;
+// int numberTwo ;
+// Console.WriteLine($"Enter number to muliply");
+//  numberOne = Convert.ToInt32(Console.ReadLine());
+// // numberOne = int.Parse(enteredNumberOne); 
+//  numberTwo = Convert.ToInt32(Console.ReadLine());
+// // numberTwo = int.Parse(enteredNumberTwo);
+// Console.WriteLine($"The Answer is "+ (numberOne*numberTwo));
+
+
+// int[] array = {10,22,33,44,55,66};
+// for (int i = 0; i < array.Length; i++)
+// {
+//     Console.Write($"{array[i]}  " );
+
+// }
+
+// try
+// {
+//     string guessWord = "giraffee";
+//     string guess = "";
+//     int times = 4;
+
+//     do
+//     {
+//         Console.Write($"Enter guess: ");
+
+//         guess = Console.ReadLine();
+
+//         times--;
+//         if (guess != guessWord)
+//         {
+//             if (times == 0)
+//             {
+//                 Console.WriteLine($"You loast the answer was " + guessWord);
+//                 break;
+
+
+//             }
+//             Console.WriteLine("You have " + times + " guess left");
+//             Console.WriteLine($"Sorry try again");
+
+
+
+//         }
+
+
+//     } while (guessWord != guess);
+
+//     if (guessWord == guess)
+//     {
+//         Console.WriteLine($"You won!");
+
+
+//     }
+// }
+// catch (Exception e)
+// {
+
+//     Console.WriteLine($"{e.Message}");
+
+// }
+    
+
+
+
+
+
+
+
+
